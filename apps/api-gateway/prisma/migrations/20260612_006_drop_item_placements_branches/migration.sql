930cbc822f713ef30e4929ecc41d37ee3286ed4a
Fatchur Rachman <arrachm4n@gmail.com>
2026-06-12 12:29:37 +0000

    chore(erp): tuntaskan penghapusan tab Lokasi/Branch item (DTO, editor, migrasi drop)

---
+++ apps/api-gateway/prisma/migrations/20260612_006_drop_item_placements_branches/migration.sql
@@ -0,0 +1,6 @@
+-- Drop item "Lokasi" (md_item_placements) & "Branch" (md_item_branches) tabs
+-- from the item master (tab removed from items form; full backend removal).
+
+DROP TABLE IF EXISTS "md_item_placements";
+
+DROP TABLE IF EXISTS "md_item_branches";
